3603248b80f07e77b0c017b9bbe9a8aa1b472b60
Mario Fehr <mario.fehr@massiveart.com>
2021-12-17 21:38:19 +0100

    add tests/unit/App.spec.ts and install jest-transform-stub

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { join } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue-ts/',
   resolve: {
     alias: {
       '@': join(__dirname, 'src'),
