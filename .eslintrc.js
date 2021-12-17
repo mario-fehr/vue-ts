e77ccb13b69c5c35e34468aa7458fd1b933bb41f
Mario Fehr <mario.fehr@massiveart.com>
2021-12-17 21:51:07 +0100

    run pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,8 +19,8 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
   overrides: [
     {
