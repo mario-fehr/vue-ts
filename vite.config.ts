c33c31d94ff4175ed11c268d34f59990b8728f27
Mario Fehr <mario.fehr@massiveart.com>
2021-12-17 21:15:22 +0100

    Project config improvements

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   base: '/vue-ts/',
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+    },
+  },
 })
