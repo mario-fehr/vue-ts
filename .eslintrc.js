3186d57fdc9952fc923b753e3da57b6e2686bd45
Mario Fehr <mario.fehr@massiveart.com>
2021-12-17 19:51:47 +0100

    setup pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-    root: true,
-    // https://eslint.org/docs/rules/no-undef#nodejs
-    env: {
-        node: true,
-        'vue/setup-compiler-macros': true
-    },
-    parser: 'vue-eslint-parser',
-    // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
-    parserOptions: {
-        parser: '@typescript-eslint/parser',
-    },
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:vue/vue3-recommended',
-        'prettier',
-    ],
+  root: true,
+  // https://eslint.org/docs/rules/no-undef#nodejs
+  env: {
+    node: true,
+    'vue/setup-compiler-macros': true,
+  },
+  parser: 'vue-eslint-parser',
+  // https://github.com/vuejs/vue-eslint-parser#parseroptionsparser
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
 }
