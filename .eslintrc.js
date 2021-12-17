de9d9b5894ba694f10eb3febdd9d39f9c8794c53
Mario Fehr <mario.fehr@massiveart.com>
2021-12-17 19:29:58 +0100

    install eslint-config-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    root: true,
-    // https://eslint.org/docs/rules/no-undef#nodejs
-    env: {
-        node: true,
-    },
-    parser: '@typescript-eslint/parser',
-    plugins: [
-        '@typescript-eslint',
-    ],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
+  root: true,
+  // https://eslint.org/docs/rules/no-undef#nodejs
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
 }
